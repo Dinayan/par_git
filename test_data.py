7f6959cc41fe0c4d94fd5a13cdf474ca5bec57e1
Dinayan <2975227646@qq.com>
2018-11-08 17:17:09 +0800

    test_data.py

---
+++ test_data.py
@@ -0,0 +1,13 @@
+#list
+list1 = ["hello", "welcome", "to", "python"]
+list1[0] = "Dina"
+print(list1)
+
+#dic
+dic1 = {"key1": "value1", "key2": "value2", "key3": "value3"}
+dic1["key4"] = "value4"
+print("dic1:", dic1)
+dic1['key1'] = 'value'
+print("after:", dic1)
+
+
